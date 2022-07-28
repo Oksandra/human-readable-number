8f8934e422166a5b636d51e16d9a394a119ca50f
Oksana <5oks5@mail.com>
2022-07-28 13:25:57 +0400

    solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
-  
+    let numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let dozens =['','', 'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    let numberByString = number.toString()
+    let result  
+    if(number === 0) {
+          return 'zero'
+        }
+    if(number > 0 && number < 20) { 
+     return result = numbers[number];
+    }
+   if(number > 19 && number < 100) { 
+    result = dozens[numberByString[0]] + ' ' + numbers[number % 10] ;
+      return result.trim()
+    }
+  if(number > 99 && number < 1000) {
+   result = numbers[numberByString[0]] + ' ' + 'hundred' + ' '
+   let trimmedNumber = numberByString.substring(1)
+   let stringNumberToCompare = +trimmedNumber
+     if (stringNumberToCompare > 0 && stringNumberToCompare < 20) {
+       result += numbers[stringNumberToCompare]}
+    if (stringNumberToCompare > 19 && stringNumberToCompare < 100) {
+       result += dozens[trimmedNumber[0]] + ' ' + numbers[number % 10] ;}
+     return result.trim()
+  }
+    
+ 
 }
+
